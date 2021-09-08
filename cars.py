5986475d1b85fa7520b31b0e47fecbc8d8387b1e
Skolens <georgijs.popovs@valmiera.edu.lv>
2021-09-08 11:11:56 +0300

    cars

---
+++ cars.py
@@ -0,0 +1,8 @@
+class Cars:
+    engine = "petrol"
+    def __init__(self,name,age,price):
+        self.name = name
+        self.age = age
+        self.price = price
+    def myfirstClassFunction(self):
+        return "This car is expensive"
--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-class Robots:
-    """"ROBOT1"""
-    def __init__(self, vards):
-        """Datu inicializācija."""
-        # Īpašības definēšana
-        self.vards = vards
-        print("(Inicializējam {})".format(self.vards))
+import cars as code
 
-        # Metode, kas izvada tekstu
-        def iznicinat(self):
-            print("{} tiek iznīcināts!".format(self.vards))
-
-        # Metode, kas izvada tekstu
-        def sasveicinaties(self):
-            print("Sveiks! Mani sauc {}.".format(self.vards))
+firstCar = code.Cars("Toyota", 2004, 5000)
+secondCar = code.Cars("BMW", 2010, 10000)
+thirdCar = code.Cars("Audi", 2018, 13500)
 
 
-        2.
-        Klases
-        pārbaude
-        un
-        reāla
-        izmantošana
-        # 1. robota izveide
-        droid1 = Robots("R2-D2")
-        # 1. robota metodes pārbaude
-        droid1.sasveicinaties()
-        # 2. robota izveide
-        droid2 = Robots("C-3PO")
-        °
-        # 2. robota metodes pārbaude
-        droid2.sasveicinaties()
+print(firstCar.name)
+print(secondCar.name)
+print(thirdCar.name)
 
-        print("\nRoboti šeit var paveikt kādu darbu..\n")
-        print("Roboti visu ir izdarījuši. Mēs viņus varam iznīcināt.")
-        droid1.iznicinat()
-        droid2.iznicinat()
+
